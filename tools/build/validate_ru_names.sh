41f03853c061501e1a20dc6949b417abff736e47
PetyaFX <134168550+PetyaFX@users.noreply.github.com>
2026-06-11 17:12:27 +0200

    Output

--- tools/build/validate_ru_names.sh
+++ tools/build/validate_ru_names.sh
@@ -6,9 +6,5 @@ HEADER=tools/translations/ru_names_header.toml
 FRAGMENTS=modular_bandastation/translations/public/ru_names
 OUTPUT=modular_bandastation/translations/public/ru_names.toml
 
-fragment_count=$(find "$FRAGMENTS" -type f -name '*.toml' | wc -l)
-validated_count=$((fragment_count + 1))
-
 tools/bootstrap/python tools/translations/merge_ru_names.py "$HEADER" "$FRAGMENTS" "$OUTPUT"
-tools/bootstrap/python tools/translations/validate_ru_names.py "$OUTPUT"
-echo "Validated $validated_count files ($fragment_count fragments + header)"
+exec tools/bootstrap/python tools/translations/validate_ru_names.py "$OUTPUT"
